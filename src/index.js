0f00f7e2c5debe4dbb050b871fbc4fee9cc011c7
Mikhail Lis <mixal8411@gmail.com>
2018-07-14 14:53:27 +0300

    feat(dev): add View class

--- src/index.js
+++ src/index.js
@@ -1,21 +1,32 @@
 
-import _ from 'lodash';
-import './style.css';
-
-function component() {
-  const element = document.createElement('div');
-
-  element.innerHTML = _.join(['Hello', 'world'], ' ');
-  element.classList.add('hello');
-
-  return element;
-}
-
-document.body.appendChild(component());
-
 class View {
-  constructor() {
-
+  constructor(selector, tagName, idName, className) {
+    this.selector = document.querySelector(selector);
+    this.tagName = tagName;
+    this.idName = idName;
+    this.className = className;
+    this.init();
   }
 
+  init() {
+    this.render();
+  }
+
+  render() {
+    this.selector.appendChild(this._createElement());
+  }
+
+  _createElement() {
+    const element = document.createElement(this.tagName);
+    element.id = this.idName;
+    this._addAttrsToELement(element, element.id, this.className);
+    return element;
+  }
+
+  _addAttrsToELement(element, id, className) {
+    const elemClassName = element.classList.add(className);
+    return elemClassName;
+  }
 }
+
+const menuContainer = new View('body', 'ul', 'nav', 'menu');
