be220cdd6753fefdf43c2ed7d9f33f67f9dcebca
Mikhail Lis <mixal8411@gmail.com>
2018-07-16 00:22:36 +0300

    feat(dev): add search input and button

--- src/index.js
+++ src/index.js
@@ -1,43 +1,6 @@
 
 import './style.css';
+import View from './View.js';
 
-class View {
-  constructor(selector, tagName, idName, className) {
-    this.selector = document.querySelector(selector);
-    this.tagName = tagName;
-    this.idName = idName;
-    this.className = className;
-    this.init();
-  }
 
-  init() {
-    this.render();
-  }
 
-  render() {
-    this.selector.appendChild(this._createElement());
-  }
-
-  _createElement() {
-    const element = document.createElement(this.tagName);
-    element.id = this.idName;
-    this._addAttrsToELement(element, element.id, this.className);
-    return element;
-  }
-
-  _addAttrsToELement(element, id, className) {
-    const elemClassName = element.classList.add(className);
-    return elemClassName;
-  }
-}
-
-class AppView extends View {
-  constructor (selector, tagName, idName, className) {
-    super (selector, tagName, idName, className);
-
-  }
-
-}
-
-const wrapper = new AppView('body', 'div', 'app', 'menu--wrapper');
-const menuContainer = new AppView('wrapper', 'ul', 'nav', 'menu');
